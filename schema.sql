0a37a22731d8071a61e520f03d30da7c171102b1
Darren Burgess <darrentburgess@gmail.com>
2016-09-13 21:59:13 -0400

    add schema

---
+++ schema.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS expenses;
+
+CREATE TABLE expenses (
+  id serial primary key,
+  amount numeric(6,2) CONSTRAINT positive_amount CHECK(amount > 0), 
+  memo text NOT NULL,
+  created_on DATE NOT NULL
+);
