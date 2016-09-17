7b4c8a152ae61432564d3e729d4916e9edf563ff
Darren Burgess <darrentburgess@gmail.com>
2016-09-17 12:11:10 -0400

    modify schema to remove drop table and add default for date

--- schema.sql
+++ schema.sql
@@ -1,8 +1,6 @@
-DROP TABLE IF EXISTS expenses;
-
 CREATE TABLE expenses (
   id serial primary key,
   amount numeric(6,2) CONSTRAINT positive_amount CHECK(amount > 0), 
   memo text NOT NULL,
-  created_on DATE NOT NULL
+  created_on DATE NOT NULL DEFAULT CURRENT_DATE
 );
